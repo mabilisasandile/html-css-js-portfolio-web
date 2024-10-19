3bc2e66d2698d44fb04a0eddada4c0e9eed0e082
mabilisasandile <mabilisasandile@gmail.com>
2024-10-19 17:49:39 +0200

    Update

--- script.js
+++ script.js
@@ -5,3 +5,34 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+// Message logic
+document.getElementById('emailForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Get form data
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+    const FormData = {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
+    }
+
+    // Send email request
+    fetch('https://my-node-app-qfg5.onrender.com/submit-form', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ FormData }),
+    })
+    .then(response => response.text())
+    .then(data => alert(data))
+    .catch(error => console.error('Error:', error));
+});
+
