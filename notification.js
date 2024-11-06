9f23863a835435586bd85c14e38f1b8245fa4beb
Sandile <mabilisasandile@gmail.com>
2024-11-06 10:31:20 +0200

    Latest updates

---
+++ notification.js
@@ -0,0 +1,30 @@
+
+async function sendNotification () {
+
+    try {
+        const FormData = {
+            name: "Sandile",
+            email: "sandile.portfolio@gmail.com",
+            subject: "Portfolio view",
+            message: "Someone visited your portfolio site."
+        }
+    
+        // Send email request
+        await fetch('https://my-node-app-qfg5.onrender.com/submit-form', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ FormData }),
+        })
+        .then(response => response.text())
+        .then(data => console.log(data))
+        
+    } catch (error) {
+        console.log("Sending notification error:", error)
+    }
+    
+}
+
+// Load data when the page loads
+window.onload = sendNotification;
